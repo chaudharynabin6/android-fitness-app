d891d13fced7799493ce9ae0e7f6d84238515aa8
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-07-04 15:01:54 +0545

    tracking service : making foreground service

--- app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
+++ app/src/main/java/com/androiddevs/runningappyt/other/Constants.kt
@@ -12,4 +12,11 @@ object Constants {
     const val ACTION_PAUSE_SERVICE = "ACTION_PAUSE_SERVICE"
     const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
 
+//    notification
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "tracking"
+    const val NOTIFICATION_ID  = 1
+    const val ACTION_SHOW_TRACKING_FRAGMENT = "ACTION_SHOW_TRACKING_FRAGMENT"
+
+
 }
--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.androiddevs.runningappyt.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
+import com.androiddevs.runningappyt.other.Constants
+import com.androiddevs.runningappyt.ui.fragments.TrackingFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateToTrackingFragmentIfNeeded(intent)
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navController = navHostFragment.findNavController())
 
@@ -32,4 +35,22 @@ class MainActivity : AppCompatActivity() {
                 }
             }
     }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+
+        intent?.let {
+            if (
+                it.action == Constants.ACTION_SHOW_TRACKING_FRAGMENT
+            ) {
+                val action = TrackingFragmentDirections.actionGlobalTrackingFragment()
+                navHostFragment.findNavController().navigate(action)
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
 }
